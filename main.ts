65333c46686da2bade7044c08098bfaf6b33146c
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-15 13:38:25 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ let value = 0
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P0)
     if (value > 500) {
-        basic.showArrow(ArrowNames.North)
+    	
     } else {
-        basic.showArrow(ArrowNames.South)
+    	
     }
 })
