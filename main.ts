7d618caef27ca28f21cb76fe09359b21a6477ae7
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-15 14:01:43 -0500

    added show value

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let value = 0
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P0)
+    basic.showNumber(value)
     if (value > 965) {
         basic.showIcon(IconNames.Happy)
     } else if (value > 955 && value < 965) {
