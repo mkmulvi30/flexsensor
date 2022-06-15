0768cd51ab0b8e3463f7189cc49db93947b57d0d
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-15 13:47:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 let value = 0
 basic.forever(function () {
     value = pins.analogReadPin(AnalogPin.P0)
-    if (value > 500) {
-    	
+    if (value > 965) {
+        basic.showIcon(IconNames.Happy)
+    } else if (value > 955 && value < 965) {
+        basic.showIcon(IconNames.No)
     } else {
-    	
+        basic.showIcon(IconNames.Asleep)
     }
 })
